f272383114df79ac4008890574cb77479c630e19
Goro Kim <31192175+CanterburyKim@users.noreply.github.com>
2019-01-23 10:33:37 -0500

    Create deter_lib.py

---
+++ deter_lib.py
@@ -0,0 +1,14 @@
+def deter2x2(matrix):
+    """
+    function to calculate the determinant of
+    the 2x2 matrix.
+    The determinant of a matrix is defined as
+    the upper-left element times the lower right element
+    minus
+    the upper-right element times the lower left element
+    """
+    result = 0
+
+    # TODO: calculate the determinant of the 2x2 matrix
+
+    return result
